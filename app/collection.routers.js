6824ee41e84b4da1317dc5e92ea4b280ec600f07
Checha Andrey <andreychecha@gmail.com>
2012-02-20 11:58:53 +0200

    rm old file

--- app/collection.routers.js
+++
@@ -1,6 +0,0 @@
-this.Tickets.CollectionRouters = Backbone.Collection.extend({
-    model: this.Tickets.ModelRouter,
-    initialize: function() {
-        
-    }
-});
--- app/model.router.js
+++
@@ -1,7 +0,0 @@
-this.Tickets.ModelRouter = Backbone.Model.extend({
-    defaults: {
-        id: 0,
-        type: '',
-        view: null
-    }
-});
